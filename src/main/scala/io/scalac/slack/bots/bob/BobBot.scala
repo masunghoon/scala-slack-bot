1399a43bc8bc669c054cf62c163e4d035320debc
마성훈 <masunghoon@ridi.com>
2017-04-13 17:52:28 +0900

    init Bob

---
+++ src/main/scala/io/scalac/slack/bots/bob/BobBot.scala
@@ -0,0 +1,65 @@
+package io.scalac.slack.bots.bob
+
+import java.io._
+
+import io.scalac.slack.MessageEventBus
+import io.scalac.slack.bots.AbstractBot
+import io.scalac.slack.common._
+
+import scala.io.Source
+import scala.util.Random
+
+case class Restaurant(name: String)
+
+/**
+  * Maintainer: Mario
+  */
+class BobBot(override val bus: MessageEventBus) extends AbstractBot {
+  val file = new File("src/main/resources/restaurants.csv")
+
+  def restaurants: Vector[Restaurant] = {
+    val inputStream = new FileInputStream(file)
+    val lines: Iterator[String] = Source.fromInputStream(inputStream).getLines
+    lines.toVector.map(Restaurant)
+  }
+
+  override def act: Receive = {
+    case Command("점심", _, message) =>
+      publish(OutboundMessage(message.channel, s"${Random.shuffle(restaurants).head.name}?!"))
+
+    case Command("list", _, message) =>
+      publish(OutboundMessage(message.channel, restaurants.map(_.name.trim).mkString(", ")))
+
+    case Command("add", params, message) =>
+      val name = params.mkString(" ")
+
+      if (restaurants.exists(_.name == name)) publish(OutboundMessage(message.channel, s"$name already exists."))
+      else {
+        val outputStream = new FileOutputStream(file, true)
+        val writer = new PrintWriter(outputStream)
+        writer.println(name)
+        writer.close()
+        publish(OutboundMessage(message.channel, s"$name added"))
+      }
+
+    case BaseMessage(text, channel, user, _, _) =>
+      BotInfoKeeper.current match {
+        case Some(bi) if text.matches("(.*)(먹자)($|(\\s+.*))") && (text.contains(bi.id) || text.contains(bi.name)) && user != bi.id =>
+          publish(OutboundMessage(channel, s"""${Random.shuffle(restaurants).head.name} GO!"""))
+
+        case Some(bi) if text.matches("(.*)아는 식당($|(\\s+.*))") && (text.contains(bi.id) || text.contains(bi.name)) && user != bi.id =>
+          publish(OutboundMessage(channel, restaurants.map(_.name).mkString(", ")))
+
+        case Some(bi) if text.matches("(?i)(^|\\s*)(hi|hello)($|(\\s+.*))") && (text.contains(bi.id) || text.contains(bi.name)) && user != bi.id =>
+          val welcomes = List("what's up?", "how's going?", "ready for work?", "nice to see you")
+          def welcome = Random.shuffle(welcomes).head
+          publish(OutboundMessage(channel, s"""hello <@$user>,\\n $welcome"""))
+
+        case _ => //nothing to do!
+      }
+
+  }
+
+  override def help(channel: String): OutboundMessage = OutboundMessage(channel, s"When you feel hungry and don't know where to go *$name* is something for you. \\n " +
+    s"`Bob` - to talk with the bot")
+}
--- src/main/scala/io/scalac/slack/common/DefaultBotBundle.scala
+++ src/main/scala/io/scalac/slack/common/DefaultBotBundle.scala
@@ -1,51 +1,53 @@
 package io.scalac.slack.common
 
 import akka.actor.{ActorContext, ActorRef, Props}
+import io.scalac.slack.bots.bob.BobBot
 import io.scalac.slack.bots.gifs.{GifsBot, GifsRepository}
 import io.scalac.slack.bots.recruitment.{EmployeeRepository, RecruitmentBot}
 import io.scalac.slack.bots.voting.{InMemoryVotingRepo, VotingBot}
-import io.scalac.slack.bots.{DirectMessageTestBot, RichMessageTestBot, ImportantMessageBot, LoggingBot}
-import io.scalac.slack.bots.digest.{DigestRepository, DigestBot}
-import io.scalac.slack.bots.feedback.{FeedbackRepository, FeedbackBot}
+import io.scalac.slack.bots.{DirectMessageTestBot, ImportantMessageBot, LoggingBot, RichMessageTestBot}
+import io.scalac.slack.bots.digest.{DigestBot, DigestRepository}
+import io.scalac.slack.bots.feedback.{FeedbackBot, FeedbackRepository}
 import io.scalac.slack.bots.hello.HelloBot
 import io.scalac.slack.bots.ping.PingPongBot
 import io.scalac.slack.bots.repl.{Repl, ReplBot}
-import io.scalac.slack.bots.system.{HelpBot, CommandsRecognizerBot}
-import io.scalac.slack.bots.tags.{TagsRepository, TagsBot}
-import io.scalac.slack.bots.twitter.{TwitterRepository, TwitterMessenger, TwitterBot}
+import io.scalac.slack.bots.system.{CommandsRecognizerBot, HelpBot}
+import io.scalac.slack.bots.tags.{TagsBot, TagsRepository}
+import io.scalac.slack.bots.twitter.{TwitterBot, TwitterMessenger, TwitterRepository}
 import io.scalac.slack.{BotModules, Config, MessageEventBus, SlackBot}
 
 class DefaultBotBundle extends BotModules {
 
-  override def registerModules(context: ActorContext, websocketClient: ActorRef) = {
+  override def registerModules(context: ActorContext, websocketClient: ActorRef): Unit = {
 
     val bus: MessageEventBus = SlackBot.eventBus
 
     val loggingBot = context.actorOf(Props[LoggingBot], "loggingBot")
     val pingpongBot = context.actorOf(Props[PingPongBot], "pingpongBot")
-    val digestBot = context.actorOf(Props(classOf[DigestBot], new DigestRepository(), bus), "digestBot")
+//    val digestBot = context.actorOf(Props(classOf[DigestBot], new DigestRepository(), bus), "digestBot")
     val commandProcessor = context.actorOf(Props(classOf[CommandsRecognizerBot], bus), "commandProcessor")
-    val helloBot = context.actorOf(Props(classOf[HelloBot], bus),  "helloBot")
-    val replBot =  context.actorOf(Props(classOf[ReplBot], new Repl(Config.scalaLibraryPath), bus), "replBot")
-    val twitterBot =  context.actorOf(
-      Props(classOf[TwitterBot],
-        Config.twitterGuardians,
-        new TwitterMessenger(Config.consumerKey,
-          Config.consumerKeySecret,
-          Config.accessToken,
-          Config.accessTokenSecret),
-        new TwitterRepository(),
-        bus),
-      "twitterBot"
-    )
-    val tagBot = context.actorOf(Props(classOf[TagsBot], new TagsRepository(), bus), "tagBot")
-    val feedbackBot = context.actorOf(Props(classOf[FeedbackBot], new FeedbackRepository(), bus), "feedbackBot")
+//    val helloBot = context.actorOf(Props(classOf[HelloBot], bus),  "helloBot")
+//    val replBot =  context.actorOf(Props(classOf[ReplBot], new Repl(Config.scalaLibraryPath), bus), "replBot")
+//    val twitterBot =  context.actorOf(
+//      Props(classOf[TwitterBot],
+//        Config.twitterGuardians,
+//        new TwitterMessenger(Config.consumerKey,
+//          Config.consumerKeySecret,
+//          Config.accessToken,
+//          Config.accessTokenSecret),
+//        new TwitterRepository(),
+//        bus),
+//      "twitterBot"
+//    )
+//    val tagBot = context.actorOf(Props(classOf[TagsBot], new TagsRepository(), bus), "tagBot")
+//    val feedbackBot = context.actorOf(Props(classOf[FeedbackBot], new FeedbackRepository(), bus), "feedbackBot")
     val helpBot = context.actorOf(Props(classOf[HelpBot], bus), "helpBot")
     val importantMessageBot = context.actorOf(Props(classOf[ImportantMessageBot], bus), "importantMessageBot")
     val richMessageBot = context.actorOf(Props[RichMessageTestBot], "richMessageBot")
-    val gifBot = context.actorOf(Props(classOf[GifsBot], new GifsRepository(), bus), "gifBot")
-    val recruitmentBot = context.actorOf(Props(classOf[RecruitmentBot], new EmployeeRepository(), bus), "recruitmentBot")
-    val directBot = context.actorOf(Props(classOf[DirectMessageTestBot], bus), "direct-message-bot")
-    val votingtBot = context.actorOf(Props(classOf[VotingBot], new InMemoryVotingRepo, bus), "votingBot")
+//    val gifBot = context.actorOf(Props(classOf[GifsBot], new GifsRepository(), bus), "gifBot")
+//    val recruitmentBot = context.actorOf(Props(classOf[RecruitmentBot], new EmployeeRepository(), bus), "recruitmentBot")
+//    val directBot = context.actorOf(Props(classOf[DirectMessageTestBot], bus), "direct-message-bot")
+//    val votingtBot = context.actorOf(Props(classOf[VotingBot], new InMemoryVotingRepo, bus), "votingBot")
+    val bobBot = context.actorOf(Props(classOf[BobBot], bus), "BobBot")
   }
 }
